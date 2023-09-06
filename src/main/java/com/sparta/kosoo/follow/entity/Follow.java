2798e04a87d51a177ec9f0ff0497e974db67205c
VerdureKo <verdure.ko@gmail.com>
2023-09-06 10:35:19 +0900

    [추가] 회원, 팔로우 entity 및 이미지 vo 구현

---
+++ src/main/java/com/sparta/kosoo/follow/entity/Follow.java
@@ -0,0 +1,35 @@
+package com.sparta.kosoo.follow.entity;
+
+import com.sparta.kosoo.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "follows")
+public class Follow {
+
+    @Id
+    @Column(name = "follow_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follow_member_id")
+    private Member followMember;
+
+    @Builder
+    public Follow(Member member, Member followMember) {
+        this.member = member;
+        this.followMember = followMember;
+    }
+
+}
---
+++ src/main/java/com/sparta/kosoo/member/entity/MemberRole.java
@@ -0,0 +1,6 @@
+package com.sparta.kosoo.member.entity;
+
+public enum MemberRole {
+    ROLE_USER, ROLE_ADMIN;
+
+}
